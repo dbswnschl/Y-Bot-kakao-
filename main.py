10a24fcd90e70f1eede7db6ed7b42795d4cf474b
dbswnschl <dbswnschl@gmail.com>
2018-06-11 22:25:23 +0900

    add TMON

---
+++ main.py
@@ -0,0 +1,210 @@
+from selenium.webdriver import PhantomJS
+from flask import Flask
+from flask import request
+import _thread
+import plugins
+import json
+import time
+global plugin
+plugin = plugins.plugin()
+
+app = Flask(__name__)
+
+
+def flaskThread(app):
+    print("start ")
+def reqcheck():
+    while 1:
+        if plugin.req == 1:
+            print("동작 시작 ondisk")
+            plugin.ondisk()
+            print("동작 종료 ondisk")
+            plugin.req = 0
+        elif plugin.req == 2:
+            print("동작 시작 TMON")
+            plugin.tmon()
+            print("동작 종료 TMON")
+            plugin.req = 0
+        time.sleep(1)
+
+
+
+@app.route('/keyboard')
+def keyboard():
+    returnobj = {
+
+        'type': 'buttons',
+        'buttons': ['요청', '최초']
+    }
+    return json.dumps(returnobj)
+@app.route('/message', methods=['POST'])
+def message():
+    data = request.data.decode('utf-8')
+    jdata = json.loads(data)
+    print(jdata)
+
+    if jdata['content'] == "요청":
+        # t = threading.Thread(target=plugin.ondisk())
+        # t.daemon = True
+        # t.start()
+
+        returnobj = {
+            'message': {
+                'text': "종류를 선택해 주세요.",
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": [
+                    "온디스크",
+                    "티켓몬스터",
+                    "최초"
+                ]}
+        }
+        return json.dumps(returnobj)
+    elif jdata['content'] == "온디스크":
+        plugin.req = 1
+        returnobj = {
+            'message': {
+                'text': "기능을 선택해 주세요.",
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": [
+                    "결과보기(온디스크)",
+                    "최초"
+                ]}
+        }
+        return json.dumps(returnobj)
+    elif jdata['content'] == "결과보기(온디스크)":
+        if plugin.req == 1:
+            txt = "요청이 진행중 입니다."
+            returnobj = {
+                'message': {
+                    'text': txt
+                },
+                "keyboard": {
+                    "type": "buttons",
+                    "buttons": [
+                        "결과보기(온디스크)",
+                        "최초"
+                    ]}
+            }
+        elif plugin.req == 0:
+            txt = plugin.ondisk_ret
+            returnobj = {
+            'message': {
+                'text': txt
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": [
+                    "티켓몬스터",
+                    "최초"
+                ]}
+            }
+
+        return json.dumps(returnobj)
+    elif jdata['content'] == "티켓몬스터":
+        plugin.req = 2
+        returnobj = {
+            'message': {
+                'text': "기능을 선택해 주세요.",
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": [
+                    "결과보기(티켓몬스터)",
+                    "최초"
+                ]}
+        }
+        return json.dumps(returnobj)
+    elif jdata['content']== "결과보기(티켓몬스터)":
+        if plugin.req == 2:
+            txt = "요청이 진행중 입니다."
+            returnobj = {
+                'message': {
+                    'text': txt
+                },
+                "keyboard": {
+                    "type": "buttons",
+                    "buttons": [
+                        "결과보기(티켓몬스터)",
+                        "최초"
+                    ]}
+            }
+        elif plugin.req == 0:
+            txt = plugin.tmon_ret
+            returnobj = {
+            'message': {
+                'text': txt
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": [
+                    "온디스크",
+                    "최초"
+                ]}
+            }
+
+        return json.dumps(returnobj)
+    elif jdata['content'] == '응답':
+        if plugin.req == 1:
+            txt = "요청이 진행중 입니다."
+        else:
+            txt = plugin.ondisk_ret
+        returnobj = {
+            'message': {
+                'text': txt
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": [
+                    "요청",
+                    "응답",
+                    "최초"
+                ]}
+        }
+
+        return json.dumps(returnobj)
+    elif jdata['content'] == "재시작":
+
+        plugin.restart()
+        returnobj = {
+            'message': {
+                'text': "재시작 하였습니다."
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": [
+                    "요청",
+
+                    "최초"
+                ]}
+
+        }
+        return json.dumps(returnobj)
+    else:
+        returnobj = {
+            'message': {
+                'text': "처음 화면 입니다."
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": [
+                    "요청",
+
+                    "최초","재시작"
+                ]}
+
+        }
+        return json.dumps(returnobj)
+
+
+
+if __name__ == '__main__':
+    _thread.start_new_thread(reqcheck, ())
+    # _thread.start_new_thread(flaskThread,(app,))
+
+    app.run(host='0.0.0.0', port=80, threaded=True)
+    while 1:
+        pass
--- plugins.py
+++ plugins.py
@@ -1,7 +1,9 @@
-from selenium.webdriver import Chrome
+from selenium.webdriver import PhantomJS
 import os
 
-
+import configparser
+config = configparser.ConfigParser()
+config.read('conf.ini')
 
 
 class plugin:
@@ -9,8 +11,10 @@ class plugin:
         APP_ROOT = os.path.dirname(os.path.abspath(__file__))
         print(APP_ROOT)
         self.req = 0
-        self.driver = Chrome(APP_ROOT+"/phantomjs.exe",service_log_path=os.path.devnull)
-
+        self.driver = PhantomJS(APP_ROOT+"/phantomjs.exe",service_log_path=os.path.devnull)
+        self.driver.implicitly_wait(3)
+    def restart(self):
+        self.__init__()
     def frame_search(self,path):
         framedict = {}
         for child_frame in self.driver.find_elements_by_tag_name('frame'):
@@ -29,48 +33,27 @@ class plugin:
         return framedict
     def tmon(self):
         self.driver.get("https://login.ticketmonster.co.kr/user/loginform?return_url=")
-        try:
-            self.driver.find_element_by_name("userid").send_keys("")
-            self.driver.find_element_by_name("password").send_keys("")
-            self.driver.find_element_by_xpath('//*[@id="loginFrm"]/a[2]').click()
-        except:
-            print("Login skip")
-        self.driver.get("http://m.benefit.ticketmonster.co.kr/promotions/page/attendance")
+        self.driver.find_element_by_name('userid').send_keys(config['ACCOUNT']['tmon_id'])
+        self.driver.find_element_by_name('password').send_keys(config['ACCOUNT']['tmon_pw'])
+        self.driver.find_element_by_xpath('//*[@id="loginFrm"]/a[2]').click()
+        self.driver.get('http://m.benefit.ticketmonster.co.kr/promotions/page/attendance?view_mode=app')
         self.driver.find_element_by_xpath('//*[@id="attn_wrap"]/div/div/div[3]/div[2]/div[1]/button').click()
-        print(self.driver.page_source)
+
+        print(self.driver.find_element_by_class_name('content').text)
+        self.tmon_ret = self.driver.find_element_by_class_name('content').text
     def ondisk(self):
-
-        self.driver.get("https://ondisk.co.kr/index.php")
-        try:
-            self.driver.find_element_by_id("mb_id").send_keys("")
-            self.driver.find_element_by_name("mb_pw").send_keys("")
-            self.driver.find_element_by_xpath('//*[@id="page-login"]/form/div[2]/p[3]/input').click()
-        except:
-            print("로그인 생략")
-        #
-        if len(self.driver.window_handles) > 1:
-            self.driver.switch_to_window(self.driver.window_handles[1])
-            self.driver.close()
-            self.driver.switch_to_window(self.driver.window_handles[0])
-        try:
-            alert = self.driver.switch_to_alert()
-            alert.accept()
-        except:
-            print("skip alert")
-
+        self.driver.get("http://ondisk.co.kr/index.php")
+        self.driver.find_element_by_xpath('//*[@id="mb_id"]').send_keys(config['ACCOUNT']['ondisk_id'])
+        self.driver.find_element_by_xpath('//*[@id="page-login"]/form/div[2]/p[2]/input').send_keys(config['ACCOUNT']['ondisk_pw'])
+        self.driver.find_element_by_xpath('//*[@id="page-login"]/form/div[2]/p[3]/input').click()
         self.driver.get("http://ondisk.co.kr/index.php?mode=eventMarge&sm=event&action=view&idx=746&event_page=1")
         self.driver.switch_to_frame(1)
+        self.driver.execute_script("window.alert = function(msg){ window.msg = msg; };")
         self.driver.find_element_by_class_name('button').click()
 
-        try:
-            alert = self.driver.switch_to_alert()
-            returntxt = alert.text
-            print(alert.text)
-            self.ret = alert.text
-            alert.accept()
-        except:
-            print("skip alert")
-            returntxt = "skip alert"
-            self.ret = returntxt
+        alert_text = self.driver.execute_script("return window.msg;")
+        print(alert_text)
+        self.ondisk_ret = alert_text
+
 
 
